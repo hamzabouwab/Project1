349e9fba018960de1f72d7ba3f0d872630dec7cc
hamza bouwab <iso.vectra@gmail.com>
2023-04-24 17:05:30 +0100

    add linksEffect.js file

---
+++ js/linksEffect.js
@@ -0,0 +1,25 @@
+const menuLinks= document.querySelectorAll(".menu .nav-item .nav-link")
+const hoverEffect= document.querySelector("span.hoverEffect")
+ function menuLinksHoverEffect(){
+   // menuLinks.forEach(link => {
+   //   link.addEventListener("mouseover",()=>{
+   //     hoverEffect.style.transform="scale(1) "
+   //     hoverEffect.style.borderRadius="6px"
+   //     hoverEffect.style.left=link.offsetLeft+"px" ;
+   //     hoverEffect.style.top=link.offsetTop+"px"
+   //     hoverEffect.style.width=link.offsetWidth+"px";
+   //     hoverEffect.style.opacity="1";
+   //   })
+   // });
+   // menuLinks.forEach(link => {
+   //   link.addEventListener("mouseleave",()=>{
+   //     hoverEffect.style.left=link.offsetLeft+"px" ;
+   //     hoverEffect.style.transform=" scale(0)";
+   //     hoverEffect.style.borderRadius="20px";
+   //     hoverEffect.style.opacity="0";
+       
+   //   })
+   // });
+   alert("")
+ }
+ export {menuLinksHoverEffect}
